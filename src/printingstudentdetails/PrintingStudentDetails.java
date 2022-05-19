b57a6142e8cfe50f1a162911c3e2838eb1f2f8ae
SimranK2004 <saini144@sheridancollege.ca>
2022-05-19 11:37:23 -0400

    version 1 of student project

---
+++ src/printingstudentdetails/PrintingStudentDetails.java
@@ -0,0 +1,29 @@
+package printingstudentdetails;
+
+public class PrintingStudentDetails {
+    public static void main(String[] args) {
+        Student[] list = new Student[3];
+		
+		Student s1 = new Student();
+		s1.setname("Khushi");
+		s1.setage(19);
+		
+		Student s2 = new Student();
+		s2.setname("Reva");
+		s2.setage(15);
+		
+		Student s3 = new Student();
+		s3.setname("Naman");
+		s3.setage(19);
+		
+		list[0] = s1;
+		list[1] = s2;
+		list[2] = s3;
+		
+		for(int i=0; i<list.length; i++)
+		{
+			System.out.println(list[i].getname() + " " + list[i].getage());
+		}
+    }
+    
+}
---
+++ src/printingstudentdetails/Student.java
@@ -0,0 +1,26 @@
+package printingstudentdetails;
+
+public class Student {
+    private String name;
+	private int age;
+	
+	public void setname(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setage(int age)
+	{
+		this.age = age;
+	}
+	
+	public String getname()
+	{
+		return name;
+	}
+	
+	public int getage()
+	{
+		return age;
+	}
+}
